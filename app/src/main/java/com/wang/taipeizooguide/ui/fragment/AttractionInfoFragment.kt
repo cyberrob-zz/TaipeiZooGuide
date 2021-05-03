508cf742532a4e136840faa57eb64dd8c0ba5f78
Robert Wang <robert.wang@nextbank.com.tw>
2021-05-03 15:13:03 +0800

    Relocate adapters and fragments to each package

--- app/src/main/java/com/wang/taipeizooguide/ui/fragment/AttractionInfoFragment.kt
+++ app/src/main/java/com/wang/taipeizooguide/ui/fragment/AttractionInfoFragment.kt
@@ -1,4 +1,4 @@
-package com.wang.taipeizooguide.ui
+package com.wang.taipeizooguide.ui.fragment
 
 import android.content.Intent
 import android.graphics.Rect
--- app/src/main/java/com/wang/taipeizooguide/ui/fragment/NotificationsFragment.kt
+++ app/src/main/java/com/wang/taipeizooguide/ui/fragment/NotificationsFragment.kt
@@ -1,4 +1,4 @@
-package com.wang.taipeizooguide.ui
+package com.wang.taipeizooguide.ui.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
